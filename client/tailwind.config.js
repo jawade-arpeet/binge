59347864723b89032e71e7efc0ecd1a1a057af7b
jawade.arpeet <jawade.arpeet@gmail.com>
2025-01-02 18:33:02 +0530

    create base tailwind theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        background: "var(--color-background)",
+        "primary-accent": "var(--color-primary-accent)",
+        "background-accent": "var(--color-background-accent)",
+      },
+    },
   },
   plugins: [],
 };
