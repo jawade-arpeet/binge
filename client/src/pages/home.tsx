ccf5a26439c8f544bb72bd83323db7c1ba02b75d
jawade.arpeet <jawade.arpeet@gmail.com>
2025-01-02 19:25:51 +0530

    add navigation header ui with search and auth buttons

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+import Button from "../components/button";
+import Input from "../components/input";
+
 function HomePage() {
-  return <main>Hello World</main>;
+  return (
+    <main>
+      <header className="px-6 py-4">
+        <nav className="flex items-center justify-between">
+          <Link className="text-xl font-semibold" to={"/"}>
+            binge
+          </Link>
+          <div className="flex items-center gap-x-4">
+            <Input className="w-24 sm:w-full" placeholder={"Search"} />
+            <Button className="hidden sm:inline-flex" variant={"ghost"}>
+              Login
+            </Button>
+            <Button>Sign up</Button>
+          </div>
+        </nav>
+      </header>
+    </main>
+  );
 }
 
 export default HomePage;
