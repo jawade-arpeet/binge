39310f30be0c4e9f95092d8e75f48d3cb864fc8b
jawade.arpeet <jawade.arpeet@gmail.com>
2025-01-09 14:46:37 +0530

    feat: create header for home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom";
+
+import Button from "../components/button.tsx";
+import Logo from "../components/logo.tsx";
+
+function Header() {
+  return (
+    <header className={"px-6 py-4"}>
+      <nav className={"flex items-center justify-between"}>
+        <Logo />
+        <div className={"flex items-center gap-x-4"}>
+          <Link to={"/sign-in"}>
+            <Button variant={"ghost"}>Login</Button>
+          </Link>
+          <Link to={"/sign-up"}>
+            <Button>Get Started</Button>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
 function Home() {
-  return <main>Home page</main>;
+  return (
+    <main>
+      <Header />
+    </main>
+  );
 }
 
 export default Home;
