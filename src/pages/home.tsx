dfd747529e33aa98c32d3ee9cf8bca6b5cef3599
jawade.arpeet <jawade.arpeet@gmail.com>
2025-01-09 21:24:07 +0530

    feat: add Hero section with movie carousel

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,9 @@
+import { useEffect, useState } from "react";
+
 import { Link } from "react-router-dom";
+
+import Movie from "../types/movies.ts";
+import supabase from "../utils/supabase.ts";
 
 import Button from "../components/button.tsx";
 import Logo from "../components/logo.tsx";
@@ -21,10 +26,49 @@ function Header() {
   );
 }
 
+function Hero() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  async function getMovies() {
+    const { data, error } = await supabase.from("movies").select("*");
+    if (error) {
+      console.log(error);
+      alert(error.message);
+    }
+    setMovies(data as Movie[]);
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <section className={"px-6"}>
+      <h1 className={"text-2xl font-semibold"}>Trending Anime</h1>
+      <div
+        className={
+          "flex items-center gap-x-4 mt-2 w-full overflow-x-scroll *:shrink-0 scrollbar-hide"
+        }
+      >
+        {movies.map((movie) => (
+          <div key={movie.id} className="w-72 h-40">
+            <img
+              src={movie.thumbnailURL}
+              className="w-full object-cover rounded-md"
+              alt="anime"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
 function Home() {
   return (
     <main>
       <Header />
+      <Hero />
     </main>
   );
 }
