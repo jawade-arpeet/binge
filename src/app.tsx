8e97a4755b28bfc420783e1a5a0b0561cb559b99
jawade.arpeet <jawade.arpeet@gmail.com>
2025-01-28 13:56:53 +0530

    feat: add and set up tanstack-query in app component

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import HomePage from "./pages/home-page";
 import AuthLayout from "./layouts/auth-layout";
@@ -6,18 +7,22 @@ import SignUpPage from "./pages/sign-up-page";
 import SignInPage from "./pages/sign-in-page";
 import ConfirmEmailPage from "./pages/confirm-email-page";
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index path="/" element={<HomePage />} />
-        <Route element={<AuthLayout />}>
-          <Route path="/sign-up" element={<SignUpPage />} />
-          <Route path="/sign-in" element={<SignInPage />} />
-          <Route path="/confirm-email" element={<ConfirmEmailPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<HomePage />} />
+          <Route element={<AuthLayout />}>
+            <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/sign-in" element={<SignInPage />} />
+            <Route path="/confirm-email" element={<ConfirmEmailPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
