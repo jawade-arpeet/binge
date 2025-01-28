2b76f6cac949ffa44e6f9c0060becda1b83289c2
jawade.arpeet <jawade.arpeet@gmail.com>
2025-01-28 14:03:57 +0530

    feat: update home-page with responsive header

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+
+import Button from "../components/ui/button";
+
 const HomePage = () => {
-  return <main>Home Page</main>;
+  return (
+    <main className="px-6 pt-4 md:pt-6">
+      <header className="mx-auto max-w-screen-xl">
+        <nav className="flex items-center justify-between">
+          <Link to={"/"} className="text-xl font-semibold">
+            binge
+          </Link>
+          <div className="flex items-center gap-x-4">
+            <Link to={"/sign-in"}>
+              <Button variant={"ghost"}>Login</Button>
+            </Link>
+            <Link to={"/sign-up"}>
+              <Button>Sign up</Button>
+            </Link>
+          </div>
+        </nav>
+      </header>
+    </main>
+  );
 };
 
 export default HomePage;
