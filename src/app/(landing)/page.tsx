19b752c020c6af7d15228d435931eb13703f27d1
jawade.arpeet@ <jawade.arpeet@gmail.com>
2025-02-12 17:33:37 +0530

    feat: add button component

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
+
+import Button from "@/components/ui/button";
+
 const Page = () => {
-  return <main>HomePage</main>;
+  return (
+    <main className={"px-4 md:px-6"}>
+      <header className={"mx-auto max-w-screen-xl py-4"}>
+        <nav className={"flex items-center justify-between"}>
+          <Link href={"/"} className={"text-xl font-semibold"}>
+            binge
+          </Link>
+          <Button>Create Account</Button>
+        </nav>
+      </header>
+    </main>
+  );
 };
 
 export default Page;
